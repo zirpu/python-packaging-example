f0c4e058df47746282061837fd5a6b8cd8e159ed
Allan Bailey <allanb@surveymonkey.com>
2013-08-15 16:07:04 -0700

    blah

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# example stolen from hpk42's devpi docs.
+import os, sys
+
+from setuptools import setup
+
+
+if __name__ == '__main__':
+
+    setup(
+        name='example',
+        description='example test project (please ignore)',
+        version='1.0',
+        author='Holger Krekel',
+        url="http://example.com",
+        author_email='holger at merlinux.eu',
+        py_modules=["example"],
+
+## i'm trying to figure out why sdist and wheel building are different for including data.
+
+        include_package_data=True,
+
+        # # with and without MANIFEST.in this doesn't work.
+        package_data={
+            'data': ['*.cfg'],
+            '': ['*.cfg'],
+        },
+
+        data_files={
+            'data': ['data/something.cfg'],
+        },
+    )
