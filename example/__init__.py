f48284f770cb3bf7d630897366322f789b1f12fd
Allan Bailey <allanb@surveymonkey.com>
2013-08-16 08:49:28 -0700

    example of using pkgutil to pull in data from package.

--- example/__init__.py
+++ example/__init__.py
@@ -1,5 +1,12 @@
 
+from __future__ import print_function, generators
+import pkgutil
+
+
+something = pkgutil.get_data(__name__, 'data/something.cfg')
+
 #
 #
 def func():
+    print("something: [{}]".format(something.strip()))
     return 42
